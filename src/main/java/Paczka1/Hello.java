19d89db3f979eefd82e287692120130560ac6e9f
sztrok <72755032+sztrok@users.noreply.github.com>
2020-10-21 22:33:13 +0200

    update

--- src/main/java/Paczka1/Hello.java
+++ src/main/java/Paczka1/Hello.java
@@ -5,10 +5,8 @@ public class Hello {
     public static void main(String[] args){
         PL polska= new PL();
         ENG anglia= new ENG();
-        ESP hiszpania= new ESP();
         System.out.println(anglia.greeting());
         System.out.println(polska.greeting());
-        System.out.println(hiszpania.greeting());
     }
 }
 
